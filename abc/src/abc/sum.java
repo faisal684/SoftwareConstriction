837ce2fe6317778a2e39c0a566e94065bf21c4f1
faisal684 <57206098+faisal684@users.noreply.github.com>
2019-10-31 10:43:11 +0500

    Add files via upload

---
+++ abc/src/abc/sum.java
@@ -0,0 +1,20 @@
+package abc;
+import java.util.*;
+
+public class sum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		int a,b;
+		System.out.print("enter first number:  ");
+		 a = scanner.nextInt();
+		 System.out.print("enter first number:  ");
+		b = scanner.nextInt();
+		int sum =a+b;
+		 System.out.print("the sum is :  "+sum);
+		
+
+	}
+
+}
